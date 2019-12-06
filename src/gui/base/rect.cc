75a4e5b130b9c1851e17a65f336be888e3e0d9d7
tanpero <3185750139@qq.com>
2019-12-07 06:37:21 +0800

    update defintions of `class Rect`

--- src/gui/base/rect.cc
+++ src/gui/base/rect.cc
@@ -4,7 +4,63 @@ BEGIN_GUI_NAMESPACE
 
 namespace base
 {
-    
+	Rect::Rect()
+	{
+	}
+	Rect::~Rect()
+	{
+	}
+	Rect::Rect(Point & lt, double width, double height)
+	{
+		_lt = lt;
+		_rt = { lt.getX() + width, lt.getY() };
+		_lb = { lt.getX(), lt.getY() + height };
+		_rb = { lt.getX() + width, lt.getY() + height };
+	}
+	Rect::Rect(Point & lt, double length)
+	{
+		*this = Rect(lt, length, length);
+	}
+	Point Rect::getLeftTop()
+	{
+		return Point();
+	}
+	Point Rect::getRightTop()
+	{
+		return Point();
+	}
+	Point Rect::getLeftBottom()
+	{
+		return Point();
+	}
+	Point Rect::getRightBottom()
+	{
+		return Point();
+	}
+	Segment Rect::getTop()
+	{
+		return Segment();
+	}
+	Segment Rect::getBottom()
+	{
+		return Segment();
+	}
+	Segment Rect::getLeft()
+	{
+		return Segment();
+	}
+	Segment Rect::getRight()
+	{
+		return Segment();
+	}
+	double Rect::getWidth()
+	{
+		return 0.0;
+	}
+	double Rect::getHeight()
+	{
+		return 0.0;
+	}
 }
 
 END_GUI_NAMESPACE
--- src/gui/base/rect.hh
+++ src/gui/base/rect.hh
@@ -7,7 +7,41 @@ BEGIN_GUI_NAMESPACE
 
 namespace base
 {
-    
+	class Rect
+	{
+	public:
+		Rect();
+		~Rect();
+
+	public:
+		Rect(Point& lt, double width, double height);
+		Rect(Point& lt, double length);
+
+	public:
+		Point getLeftTop();
+		Point getRightTop();
+		Point getLeftBottom();
+		Point getRightBottom();
+
+	public:
+		Segment getTop();
+		Segment getBottom();
+		Segment getLeft();
+		Segment getRight();
+		double getWidth();
+		double getHeight();
+		inline std::string toString()
+		{
+			return "Left Top: " + lt.toString() + " Right Top: " + rt.toString()
+				+ "\nLeft Bottom: " + lb.toString() + " Right Bottom: " + rb.toString();
+		}
+
+	private:
+		Point _lt;
+		Point _rt;
+		Point _lb;
+		Point _rb;
+	};
 }
 
 END_GUI_NAMESPACE
